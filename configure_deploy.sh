7ac351ca9d817210c91109b3eb537eacbaf3df3e
miui <ft.nudt@foxmail.com>
2016-12-01 13:25:53 +0000

    fix some bugs

--- configure_deploy.sh
+++ configure_deploy.sh
@@ -3,29 +3,31 @@ project_path="~/workspace/keychainserver"
 reset_mysql_pass_sql_file="reset_mysql_pass.sql"
 public_key_file="public_key_py.pem"
 private_key_file="private_key_py.pem"
-sudo su
+VirtualHost_File="001-keychainserver.conf"
+Http_Proxy_File="http-proxy.conf"
+
 cd /etc/apache2/sites-enabled
 
-if [ -d 001-keychainserver.conf ]; then
-    rm 001-keychainserver.conf
+if [ -d $VirtualHost_File ]; then
+    sudo rm -i $VirtualHost_File
 fi
 
-echo "<VirtualHost *:8080>">>001-keychainserver.conf
-echo "ServerName https://${C9_HOSTNAME}:443">>001-keychainserver.conf
-echo "ProxyPass / http://localhost:8000/">>001-keychainserver.conf
-echo "ProxyPassReverse / http://localhost:8000/">>001-keychainserver.conf
-echo "ErrorLog ${APACHE_LOG_DIR}/error.log">>001-keychainserver.conf
-echo "CustomLog ${APACHE_LOG_DIR}/access.log combined">>001-keychainserver.conf
-echo "</VirtualHost>">>001-keychainserver.conf
+sudo echo "<VirtualHost *:8080>">>$VirtualHost_File
+sudo echo "ServerName https://${C9_HOSTNAME}:443">>$VirtualHost_File
+sudo echo "ProxyPass / http://localhost:8000/">>$VirtualHost_File
+sudo echo "ProxyPassReverse / http://localhost:8000/">>$VirtualHost_File
+sudo echo "ErrorLog ${APACHE_LOG_DIR}/error.log">>$VirtualHost_File
+sudo echo "CustomLog ${APACHE_LOG_DIR}/access.log combined">>$VirtualHost_File
+sudo echo "</VirtualHost>">>$VirtualHost_File
 
 cd /etc/apache2/conf-enabled
 
-if [ -d http-proxy.conf ]; then
-    rm http-proxy.conf
+if [ -d $Http_Proxy_File ]; then
+    sudo rm -i $Http_Proxy_File
 fi
 
-echo "LoadModule proxy_module /usr/lib/apache2/modules/mod_proxy.so"
-echo "LoadModule proxy_http_module /usr/lib/apache2/modules/mod_proxy_http.so"
+sudo echo "LoadModule proxy_module /usr/lib/apache2/modules/mod_proxy.so">>$Http_Proxy_File
+sudo echo "LoadModule proxy_http_module /usr/lib/apache2/modules/mod_proxy_http.so">>$Http_Proxy_File
 
 exit
 
@@ -41,6 +43,7 @@ if [ "$RESET_MYSQL_PASS" == "y" ] || [ "$RESET_MYSQL_PASS" == "" ]; then
     echo "update user set Password=PASSWORD('$PASSWORD') where USER='root';">>$reset_mysql_pass_sql_file
     echo "flush privileges;">>$reset_mysql_pass_sql_file
     sudo mysqld_safe --skip-grant-table source $reset_mysql_pass_sql_file
+    rm $reset_mysql_pass_sql_file
     echo "Your mysql root password has been reset"
 else
     echo "Skiped reset mysql root password"
@@ -67,6 +70,18 @@ mkdir -p ./keychain/static/keychain/cache/qrcode/*
 rm -rf ./keychain/migrations/0*
 python3 ./manage.py makemigrations
 python3 ./manage.py migrate
-service mysql restart
-service apache2 restart
+sudo service mysql restart
+sudo service apache2 restart
 cd $current_path
+
+echo "Your server is ready to running"
+echo "Would you like to run server? (y/n) [y]"
+read RUN_SERVER
+if [ "$RUN_SERVER" == "y" ] || [ "$RUN_SERVER" == "" ]; then
+    cd $project_path
+    sudo python3 manage.py runserver localhost:8000
+else
+    echo "Usage:"
+    echo "$ cd $project_path"
+    echo "$ sudo python3 manage.py runserver localhost:8000"
+fi
